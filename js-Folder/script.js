72f9b4e9a2bd03b927ff403b1c1fb34dd30e00c3
Erika Rivera <rivera.erika07@gmail.com>
2022-06-24 13:22:21 -0400

    puesdo code

---
+++ js-Folder/script.js
@@ -0,0 +1,13 @@
+// What does my game need?
+//a spaceship and a enemy
+//an attack for both sides
+//this will a turn base type of fighting
+//if the spaceship gets destroyed its game over
+//if the enemy is defeated there will two options retreat or attack the next opponent
+//retreat and then its game over
+//set an amount of enemies that needs to be defeated
+//optional: maybe add a countdown for how many enemies are remaining
+//when all enemies are defeated show a 'You Win!' screen 
+//suggestion: maybe see if a restart option for when you when the game
+//if you lose show a 'You Were Defeated' and 'Try Again?'
+//      
