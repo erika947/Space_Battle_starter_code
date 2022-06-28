da26786d6dcfdc7907dce2362e753aea82c16732
Erika Rivera <rivera.erika07@gmail.com>
2022-06-28 18:23:53 -0400

    update

--- js-Folder/script.js
+++ js-Folder/script.js
@@ -10,54 +10,54 @@
 //Hull is also the health
 //
 
-class Ship {
-    constructor(hull, firepower, accuracy){
-        this.hull = hull;
-        this.firepower = firepower;
-        this.accuracy = accuracy;
-    }
+// class Ship {
+//     constructor(hull, firepower, accuracy){
+//         this.hull = hull;
+//         this.firepower = firepower;
+//         this.accuracy = accuracy;
+//     }
 
-attack() {
-let randomAttack = math.random()
-    if (randomAttack <= this.accuracy){
-        alert("It's a Hit!");
-        target.hull = target.hull - this.firepower;
-        alert("Almost got them!");
-    }
-    else if
-        (target.hull >= 0){
-            target.iSurived = false;
-    }
-    else {
-        alert('missed T_T')
-        }
-    }
-}
+// attack() {
+// let randomAttack = math.random()
+//     if (randomAttack <= this.accuracy){
+//         alert("It's a Hit!");
+//         target.hull = target.hull - this.firepower;
+//         alert("Almost got them!");
+//     }
+//     else if
+//         (target.hull >= 0){
+//             target.iSurived = false;
+//     }
+//     else {
+//         alert('missed T_T')
+//         }
+//     }
+// }
 
-class Enemy {
-    constructor(hull,firepower,accuracy){
-        this.hull = math.floor(math.random() * 4) + 3 ;  //how do I give random stats?
-        this.firepower = math.floor(math.random() * 3) + 2;
-        this.accuracy = (math.floor(math.random() * 3) + 6) / 10 ;
-    }
+// class Enemy {
+//     constructor(hull,firepower,accuracy){
+//         this.hull = math.floor(math.random() * 4) + 3 ;  //how do I give random stats?
+//         this.firepower = math.floor(math.random() * 3) + 2;
+//         this.accuracy = (math.floor(math.random() * 3) + 6) / 10 ;
+//     }
 
-attack () {
-let randomAttack = math.random();
-    if (randomAttack <= this.accuracy){
-        alert("I'm Hit!");
-        target.hull = target.hull - this.firepower;
-        alert("Ship has been badly damaged!");
-    }
-    else if
-        (target.hull <= 0){
-            target.iSurived = false;
-            alert("You Were Defeated!")
-        }
-}
-}
+// attack () {
+// let randomAttack = math.random();
+//     if (randomAttack <= this.accuracy){
+//         alert("I'm Hit!");
+//         target.hull = target.hull - this.firepower;
+//         alert("Ship has been badly damaged!");
+//     }
+//     else if
+//         (target.hull <= 0){
+//             target.iSurived = false;
+//             alert("You Were Defeated!")
+//         }
+// }
+// }
 
-const heroShip = new Ship('USS Schwarzenegger', 20, 5, .7)
-const alienShip = []
+// const heroShip = new Ship('USS Schwarzenegger', 20, 5, .7)
+// const alienShip = []
   
 
 
