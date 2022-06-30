cae91ea5b3a22629f17cd983bf28a210bb4e20de
Erika Rivera <rivera.erika07@gmail.com>
2022-06-30 00:45:40 -0400

    next update

--- js-Folder/script.js
+++ js-Folder/script.js
@@ -1,67 +1,87 @@
-// What does my game need?
-//a spaceship and a enemy
-//an attack for both sides
-//if the spaceship gets destroyed its game over
-//if the enemy is defeated there will two options retreat or attack the next opponent
-//retreat and then its game over
-//set an amount of enemies that needs to be defeated
-//if you lose show a 'You Were Defeated' and 'Try Again?'
-//if the player takes damage but defeats the enemy and chooses to continue then the players health will not heal 
-//Hull is also the health
-//
+// // What does my game need?
+// //a spaceship and a enemy
+// //an attack for both sides
+// //if the spaceship gets destroyed its game over
+// //if the enemy is defeated there will two options retreat or attack the next opponent
+// //retreat and then its game over
+// //set an amount of enemies that needs to be defeated
+// //if you lose show a 'You Were Defeated' and 'Try Again?'
+// //if the player takes damage but defeats the enemy and chooses to continue then the players health will not heal 
+// //Hull is also the health
+// //
+// prompt('Welcome!' ,'ENTER TO JOIN THE BATTLE')
+// class Ship {
+//     constructor(hull, firepower, accuracy, iSurived){
+//         this.hull = hull;
+//         this.firepower = firepower;
+//         this.accuracy = accuracy;
+//         this.iSurived = iSurived;
+//     }
 
-class Ship {
-    constructor(hull, firepower, accuracy){
-        this.hull = hull;
-        this.firepower = firepower;
-        this.accuracy = accuracy;
-    }
+// attack() {
+// let randomAttack = math.random()
+//     if (randomAttack <= this.accuracy){
+//         prompt("It's a Hit!");
+//         rival.hull = rival.hull - this.firepower;
+//         prompt("Almost got them!");
+//     }
+//     else if
+//         (rival.hull >= 0){
+           
+//     }
+//     else { rival.iSurived = false;
+//         prompt('missed T_T')
+//         }
 
-attack() {
-let randomAttack = math.random()
-    if (randomAttack <= this.accuracy){
-        alert("It's a Hit!");
-        target.hull = target.hull - this.firepower;
-        alert("Almost got them!");
-    }
-    else if
-        (target.hull >= 0){
-            target.iSurived = false;
-    }
-    else {
-        alert('missed T_T')
-        }
-    }
-}
+//     }
+// }
 
-class Enemy {
-    constructor(hull,firepower,accuracy){
-        this.hull = math.floor(math.random() * 4) + 3 ;  //how do I give random stats?
-        this.firepower = math.floor(math.random() * 3) + 2;
-        this.accuracy = (math.floor(math.random() * 3) + 6) / 10 ;
-    }
+// class Enemy {
+//     constructor(hull,firepower,accuracy,iSurived){
+//         this.hull = hull ;  
+//         this.firepower = firepower;
+//         this.accuracy =  accuracy ;
+//         this.iSurived = iSurived;
+//     }
 
-attack () {
-let randomAttack = math.random();
-    if (randomAttack <= this.accuracy){
-        alert("I'm Hit!");
-        target.hull = target.hull - this.firepower;
-        alert("Ship has been badly damaged!");
-    }
-    else if
-        (target.hull <= 0){
-            target.iSurived = false;
-            alert("You Were Defeated!")
-        }
-}
-}
+// attack (rival) {
+// let randomAttack = math.random();
+//     if (randomAttack <= this.accuracy){
+//         prompt("I'm Hit!");
+//         rival.hull = rival.hull - this.firepower;
+//         prompt("Ship has been badly damaged!");
+//     }
+//         else if
+//             (rival.hull <= 0){
+//     }
+//         else{ rival.iSurived = false;   
+//             prompt("You Were Defeated!")}
+// }
+// }
+// // make something that we active the game 
+// //add a new object so that we can trigger the first prompt
 
-const heroShip = new Ship('USS Schwarzenegger', 20, 5, .7)
-const alienShip = []
-  
-
+// let gameMenu = { // another object
+//     rivalShip: 0,
+//     round: 0,
+//     userChoice: ""
+// };
 
 
+// const enemyShip = [];
+// //this will be the different options the randomizer will get to choose from
+// const enemiesHull = [4,5,6];
+// const enemiesPower = [1,2,3];
+// const enemiesAccuracy = [.3,.4,.5,.6];
 
 
-
+// let newEnemyShip = () => {
+//     //set the amount of enemy mobs being made
+//     for (let e = 0; e < 6; e++){
+//         //now lets randomize the health, firepower and accuracy
+//         let hull = enemiesHull[math.floor(math.random() * 3)];
+//         let firepower = enemiesPower[math.floor(math.random() * 3)];
+//         let accuracy = enemiesAccuracy[math.floor(math.random() * 4)];
+//         enemyShip[e] = new enemyShip(hull,firepower,accuracy);
+//     }
+// }
